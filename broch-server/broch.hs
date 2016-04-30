db0d2cadd4c6f7d46614bc5907e09e050e7eb3a0
Luke Taylor <tekul.hs@gmail.com>
2016-04-30 20:50:47 +0100

    Add SQLite backend using sqlite-simple
    Copied from Postgres version with a lot of
    duplication, but makes the two options more
    consistent and simplifies server executable code.

--- broch-server/broch.hs
+++ broch-server/broch.hs
@@ -1,32 +1,28 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 
-import Control.Exception hiding (Handler)
 import Control.Monad (msum, when)
-import Control.Monad.Logger (runNoLoggingT)
 import Crypto.KDF.BCrypt (validatePassword)
 import qualified Data.ByteArray.Encoding as BE
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BC
-import Data.Pool (createPool)
+import Data.Pool (createPool, withResource)
 import Data.Maybe (fromMaybe, isNothing)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
-import Database.Persist.Sqlite (createSqlitePool)
 import Database.PostgreSQL.Simple
+import qualified Database.SQLite.Simple as SQLite
 import Network.Wai.Application.Static
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Handler.Warp
 import Options.Applicative
-import System.Directory
 import System.Environment (getEnvironment)
 import System.Exit (die)
-import System.IO.Error
 import Web.Routing.TextRouting
 
-import Broch.PostgreSQL
+import qualified Broch.PostgreSQL as BP
+import qualified Broch.SQLite as BS
 import Broch.Server
 import Broch.Server.Config
-import Broch.Test
 import Broch.Server.Internal
 import Broch.Server.Session (defaultKey, defaultLoadSession)
 
@@ -35,7 +31,7 @@ data BackEnd = POSTGRES | SQLITE deriving (Read, Show)
 data BrochOpts = BrochOpts
     { issuer  :: T.Text
     , port    :: Int
-    , connStr :: T.Text
+    , connStr :: String
     , webRoot :: FilePath
     , backEnd :: BackEnd
     }
@@ -63,9 +59,9 @@ parser issuer db port webroot = BrochOpts
        <> metavar "PORT"
        <> value port
        <> help "The port to listen on")
-    <*> textOption
+    <*> strOption
         ( long "connection-string"
-       <> help "The postgresql connection string"
+       <> help "The postgresql connection string or sqlite database file name"
        <> metavar "DATABASE"
        <> value db)
     <*> strOption
@@ -80,13 +76,20 @@ main = do
     env <- getEnvironment
     let issuer  = fromMaybe "http://localhost:3000" $ lookup "ISSUER" env
         port    = maybe 3000 read                   $ lookup "PORT" env
-        db      = fromMaybe "dbname=broch"          $ lookup "DATABASE" env
+        db      = fromMaybe "default"               $ lookup "DATABASE" env
         webroot = fromMaybe "webroot"               $ lookup "WEBROOT" env
-        desc    = fullDesc <> progDesc "Run a standalone OpenID Connect server"
+        desc    = fullDesc <> progDesc "Run an OpenID Connect server"
     sidSalt <- decodeSalt $ lookup "SUBJECT_ID_SALT" env
-    opts <- execParser (info (helper <*> parser issuer db port webroot) desc)
+    opts <- setConnStr <$> execParser (info (helper <*> parser issuer db port webroot) desc)
     when (isNothing sidSalt) $ putStrLn "Subject identifiers will be shared between clients. Set SUBJECT_ID_SALT to use pairwise identifiers)"
     runWithOptions opts sidSalt
+  where
+    setConnStr opts
+        | connStr opts == "default" = opts { connStr = defaultConnStr (backEnd opts)}
+        | otherwise = opts
+    defaultConnStr be = case be of
+        POSTGRES -> "dbname=broch"
+        SQLITE   -> "broch.db3"
 
 decodeSalt :: Maybe String -> IO (Maybe ByteString)
 decodeSalt Nothing = return Nothing
@@ -99,37 +102,27 @@ decodeSalt (Just s) = case bs of
 runWithOptions :: BrochOpts -> Maybe ByteString -> IO ()
 runWithOptions BrochOpts {..} sidSalt = do
     sessionKey <- defaultKey
-    router <- case backEnd of
-        POSTGRES -> postgresqlConfig issuer (TE.encodeUtf8 connStr) sidSalt
-        SQLITE   -> sqliteConfig issuer
-    let broch = routerToMiddleware (defaultLoadSession 3600 sessionKey) issuer router
-        app   = staticApp (defaultWebAppSettings "webroot")
-    run port (logStdoutDev (broch app))
-
-postgresqlConfig :: T.Text -> ByteString -> Maybe ByteString -> IO (RoutingTree (Handler ()))
-postgresqlConfig issuer connStr sidSalt = do
-    pool <- createPool createConn close 1 60 20
     kr <- defaultKeyRing
     rotateKeys kr True
-    config <- postgreSQLBackend pool <$> inMemoryConfig issuer kr sidSalt
-    let baseRouter = brochServer config defaultApprovalPage authenticatedSubject authenticateSubject
-        authenticate username password = passwordAuthenticate pool validatePassword username (TE.encodeUtf8 password)
+    (mkBackEnd, passwordAuthenticate) <- case backEnd of
+        POSTGRES -> do
+            pool <- createPool (connectPostgreSQL (BC.pack connStr)) close 1 60 20
+            return (BP.postgreSQLBackend pool, BP.passwordAuthenticate pool)
+        SQLITE   -> do
+            pool <- createPool (SQLite.open connStr) SQLite.close 1 60 20
+            withResource pool $ \c -> BS.createSchema c
+            return (BS.sqliteBackend pool, \v u p -> withResource pool $ \c -> BS.passwordAuthenticate c v u p)
+
+    config <- mkBackEnd <$> inMemoryConfig issuer kr sidSalt
+    let app = staticApp (defaultWebAppSettings "webroot")
+        baseRouter = brochServer config defaultApprovalPage authenticatedSubject authenticateSubject
+        authenticate username password = passwordAuthenticate validatePassword username (TE.encodeUtf8 password)
         extraRoutes =
             [ ("/home",   text "Hello, I'm the home page")
             , ("/login",  passwordLoginHandler defaultLoginPage authenticate)
             , ("/logout", invalidateSession >> complete)
             ]
-    return $ foldl (\tree (r, h) -> addToRoutingTree r h tree) baseRouter extraRoutes
-  where
-    createConn = connectPostgreSQL connStr
+        router = foldl (\tree (r, h) -> addToRoutingTree r h tree) baseRouter extraRoutes
+        broch = routerToMiddleware (defaultLoadSession 3600 sessionKey) issuer router
 
-
-sqliteConfig :: T.Text -> IO (RoutingTree (Handler ()))
-sqliteConfig issuer = do
-    removeFile "broch.db3" `catch` eek
-    pool   <- runNoLoggingT $ createSqlitePool "broch.db3" 2
-    testBroch issuer pool
-  where
-    eek e
-      | isDoesNotExistError e = return ()
-      | otherwise             = throwIO e
+    run port (logStdoutDev (broch app))
