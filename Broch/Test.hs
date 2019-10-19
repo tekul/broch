ae5585515cb8772dc99602e423476d4a9b3d96d4
Luke Taylor <tekul.hs@gmail.com>
2019-10-19 22:51:47 +0100

    Remove test dependency on SQLite
    Probably going to remove or archive SQLite support at some point.
    This converts the test server to just use the in-memory configuration
    with simple authentication and userInfo endpoint implementations
    (the only things which were being provided by SQLite).

--- Broch/Test.hs
+++ Broch/Test.hs
@@ -2,11 +2,8 @@
 
 module Broch.Test where
 
-import           Crypto.KDF.BCrypt (validatePassword)
-import           Data.Pool (createPool, withResource)
 import           Data.Text (Text)
 import           Data.Text.Encoding (encodeUtf8)
-import           Database.SQLite.Simple as SQLite
 import qualified Web.Routing.Combinators as R
 import qualified Web.Routing.SafeRouting as R
 
@@ -14,27 +11,57 @@ import           Broch.Model
 import           Broch.Server (brochServer, defaultLoginPage,  defaultApprovalPage, authenticatedSubject, authenticateSubject, passwordLoginHandler)
 import           Broch.Server.Internal
 import           Broch.Server.Config
-import           Broch.SQLite (createSchema, sqliteBackend, passwordAuthenticate)
-import           Broch.Test.Data
+import           Broch.Test.Data (testClients)
 
 
 testBroch :: Text -> IO (R.PathMap (Handler ()))
 testBroch issuer = do
-    pool <- createPool (SQLite.open "file::memory:?cache=shared") SQLite.close 1 60 20
-    withResource pool $ \c -> createSchema c
     kr <- defaultKeyRing
-    config <- sqliteBackend pool <$> inMemoryConfig issuer kr Nothing
+    config <- inMemoryConfig issuer kr Nothing
     mapM_ (createClient config) testClients
     rotateKeys kr True
     -- Allow everything for test options
-    let testConfig = config { responseTypesSupported = map snd responseTypes }
-        passAuth v u p = withResource pool $ \c -> passwordAuthenticate c v u p
+    let passAuth u p = case (u, p) of
+           ("cat", "cat") -> return (Just "1234_cat_id")
+           _ -> return Nothing
 
-        authenticate username password = passAuth validatePassword username (encodeUtf8 password)
+        loadUserInfo :: LoadUserInfo IO
+        loadUserInfo "1234_cat_id" _ = return (Just catUser)
+        loadUserInfo sid _ = print sid >> return Nothing
+
+        testConfig = config { responseTypesSupported = map snd responseTypes, getUserInfo = loadUserInfo }
+
+        authenticate username password = passAuth username (encodeUtf8 password)
         extraRoutes =
             [ ("/home",   text "Hello, I'm the home page")
             , ("/login",  passwordLoginHandler defaultLoginPage authenticate)
             , ("/logout", invalidateSession >> complete)
             ]
         routingTable = foldl (\pathMap (r, h) -> R.insertPathMap' (R.toInternalPath (R.static r)) (const h) pathMap) (brochServer testConfig defaultApprovalPage authenticatedSubject authenticateSubject) extraRoutes
+    u <- loadUserInfo  "1234_cat_id" (head testClients) :: IO (Maybe UserInfo)
+    print u
     return routingTable
+
+catUser :: UserInfo
+catUser = UserInfo {
+    sub = "1234_cat_id"
+  , name = Just "Catherine De Feline"
+  , given_name = Just "Catherine"
+  , family_name = Just "De Feline"
+  , middle_name = Just "Kitty"
+  , nickname = Just "Cat"
+  , preferred_username = Just "cat"
+  , profile = Just "http://placeholder"
+  , picture = Just "http://placeholder"
+  , website = Just "http://placeholder"
+  , email = Just "cat@connect.broch.io"
+  , email_verified = Just False
+  , gender = Just "female"
+  , birthdate = Just "1985-7-23"
+  , zoneinfo = Just "Europe/Paris"
+  , locale = Just "fr-FR"
+  , phone_number = Just "+33 12 34 56 78"
+  , phone_number_verified = Just False
+  , address = Nothing
+  , updated_at = Nothing
+  }
