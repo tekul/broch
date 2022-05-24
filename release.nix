c747c492f2d4d05e5337e32c4ee497132b0eca9f
Jappie Klooster <jappieklooster@hotmail.com>
2022-05-24 12:11:15 +0200

    Run cabal2nix automatically, no need to commit the result of that.
    I ran into this because it couldn't find lib in stdenv,
    when generated these issues shouldn't occur, because nixpkg
    maintainers deal with that.
    
    This also upgrades to the latest nixpkgs

--- release.nix
+++ release.nix
@@ -1,14 +1,30 @@
 { compiler ? "default" }:
 
 let
-  pkgs = import <nixpkgs> {};
+  rev = "a790b646e0634695782876f45d98f93c38ceae1d";
+  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  pkgs = import (builtins.fetchTarball url) {};
   dontCheck = pkgs.haskell.lib.dontCheck;
+  ignore =
+  let
+    owner = "hercules-ci";
+    repo = "gitignore";
+    rev = "c4662e662462e7bf3c2a968483478a665d00e717";
+  in
+    import (builtins.fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    sha256 = "sha256:1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
+  }) { inherit (pkgs) lib; };
+
   hPkgs = if compiler == "default"
               then pkgs.haskellPackages
               else pkgs.haskell.packages.${compiler};
 
+  src = ignore.gitignoreSource ./.;
+  lib = pkgs.haskell.lib;
   haskellPkgs = hPkgs.extend (self: super: {
-    broch = self.callPackage ./broch.nix {};
+    jose-jwt = lib.markUnbroken super.jose-jwt;
+    broch = self.callCabal2nix "broch" src {};
   });
 in
   {
